5bf37190507d5616f163e145f0a27aa407fe1bb6
daiki-kaneda <daiki0622developing@gmail.com>
2026-02-25 22:59:07 +0900

    feat: implement minimal addtion logic

--- src/main/java/com/example/unit_test_practice/Calculator.java
+++ src/main/java/com/example/unit_test_practice/Calculator.java
@@ -1,7 +1,7 @@
 package com.example.unit_test_practice;
 
 public class Calculator {
-    public int add(int a,int b){
-        return 0;
+    public int add(int a, int b) {
+        return a + b;
     }
 }
