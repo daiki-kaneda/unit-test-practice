72063f57086b096944d210be294edd62faeec669
daiki-kaneda <daiki0622developing@gmail.com>
2026-02-25 23:33:02 +0900

    feat: add minimal string reversal implementation

--- src/main/java/com/example/unit_test_practice/StringReversal.java
+++ src/main/java/com/example/unit_test_practice/StringReversal.java
@@ -2,9 +2,13 @@ package com.example.unit_test_practice;
 
 import java.util.function.UnaryOperator;
 
-public class StringReversal implements UnaryOperator<String>{
+public class StringReversal implements UnaryOperator<String> {
     @Override
     public String apply(String t) {
-        return null;
+        if (t == null)
+            return null;
+        return new StringBuilder(t)
+                .reverse()
+                .toString();
     }
 }
