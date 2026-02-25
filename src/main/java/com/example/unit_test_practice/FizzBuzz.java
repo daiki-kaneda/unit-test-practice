ed276d717aec7bf53f77a291d84a1e9eb6006ebf
daiki-kaneda <daiki0622developing@gmail.com>
2026-02-25 23:18:20 +0900

    feat: add minimal fizzbuzz implementation

--- src/main/java/com/example/unit_test_practice/FizzBuzz.java
+++ src/main/java/com/example/unit_test_practice/FizzBuzz.java
@@ -2,8 +2,15 @@ package com.example.unit_test_practice;
 
 import java.util.function.Function;
 
-public class FizzBuzz implements Function<Integer,String>{
+public class FizzBuzz implements Function<Integer, String> {
     public String apply(Integer n) {
-        return "";
+        if (n % 15 == 0)
+            return "FizzBuzz";
+        if (n % 3 == 0)
+            return "Fizz";
+        if (n % 5 == 0)
+            return "Buzz";
+
+        return String.valueOf(n);
     }
 }
