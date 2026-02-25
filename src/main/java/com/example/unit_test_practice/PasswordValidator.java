6bc96d535265bd859053bf20feebd21e5b1ff159
daiki-kaneda <daiki0622developing@gmail.com>
2026-02-26 00:06:20 +0900

    feat: add minimal password validator implementation

--- src/main/java/com/example/unit_test_practice/PasswordValidator.java
+++ src/main/java/com/example/unit_test_practice/PasswordValidator.java
@@ -5,6 +5,15 @@ import java.util.function.Predicate;
 public class PasswordValidator implements Predicate<String>{
     @Override
     public boolean test(String t) {
+        if(t==null){
+            return false;
+        }
+        boolean hasUpperCase = t.chars().anyMatch(Character::isUpperCase);
+        boolean hasNumber = t.chars().anyMatch(Character::isDigit);
+        boolean hasAlphabetic = t.chars().anyMatch(Character::isAlphabetic);
+        if(hasUpperCase && hasNumber && hasAlphabetic){
+            return true;
+        }
         return false;
     }
 }
