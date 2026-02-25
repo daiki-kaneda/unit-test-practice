68bef9ab0af49d751c0033bcb08a3e3b46f55a8a
daiki-kaneda <daiki0622developing@gmail.com>
2026-02-26 00:23:01 +0900

    test: add calculator div test

--- src/main/java/com/example/unit_test_practice/Calculator.java
+++ src/main/java/com/example/unit_test_practice/Calculator.java
@@ -4,4 +4,8 @@ public class Calculator {
     public int add(int a, int b) {
         return a + b;
     }
+
+    public int div(int a, int b){
+        return 0;
+    }
 }
--- src/test/java/com/example/unit_test_practice/CalculatorTests.java
+++ src/test/java/com/example/unit_test_practice/CalculatorTests.java
@@ -1,5 +1,7 @@
 package com.example.unit_test_practice;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -26,5 +28,36 @@ public class CalculatorTests {
         int result = calculator.add(a, b);
         assertThat(result)
                 .isEqualTo(expected);
+    }
+
+    @ParameterizedTest(name = "{0}/{1}が{2}になること")
+    @CsvSource({
+            "4,2,2",
+            "5,2,2",
+            "-6,2,-3",
+            "-10,-3,3",
+            "0,1,0",
+    })
+    void div_VariousNumbers_ReturnsExpectedQuotient(
+            int a, int b, int expected) {
+        int result = calculator.div(a, b);
+        assertThat(result)
+                .isEqualTo(expected);
+    }
+
+    @ParameterizedTest(name = "0での除算はエラーを返すこと")
+    @CsvSource({
+        "3",
+        "0",
+        "-1"
+    })
+    void div_DivideByZero_ThrowsIllegalArgumentException(
+        int input
+    ){
+        assertThrows(
+            IllegalArgumentException.class, 
+            ()->{
+                calculator.div(1, 0);
+            });
     }
 }
