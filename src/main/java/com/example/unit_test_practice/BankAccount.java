fdc7e7b52361fe8e843e43d27f967750f7c858f5
daiki-kaneda <daiki0622developing@gmail.com>
2026-02-26 00:59:24 +0900

    feat: add minimal bank account implementation

--- src/main/java/com/example/unit_test_practice/BankAccount.java
+++ src/main/java/com/example/unit_test_practice/BankAccount.java
@@ -4,11 +4,12 @@ public class BankAccount {
     private int balance;
 
     public void withDraw(int amount) {
-
+        if(amount>balance)throw new IllegalStateException("残高が不足しています。");
+        balance -= amount;
     }
 
     public void deposit(int amount) {
-
+        balance += amount;
     }
 
     public int getBalance() {
